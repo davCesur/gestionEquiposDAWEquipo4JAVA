69baeb555c4973658b83f68b8e2db2bed80d48b7
mkarmonas <miguelangel.s@260606@cesurformacion.com>
2022-01-25 12:34:09 +0100

    Añadidas condiciones de cada entrada para Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -4,30 +4,46 @@ public class Jugador {
 	private int edad;
 	private String idioma;
 	
+	/**
+	 * Constructor
+	 */
 	public Jugador () {
 			
-		}
+	}
 	
 	public void setNombreJugador(String nombre) {
-		//code here
+		/*
+		 * Recibe un texto de máximo 20 carácteres.
+		 * No permite caracteres numéricos.
+		 * No puede estar vacío. 
+		 */
+		this.nombre = nombre;
 	}
 	
 	public void setEdad(int edad) {
-		//code here
+		/* Sólo numérico.
+		 * Debe ser mayor de edad (>18).
+		 * No puede estar vacío.
+		 */
+		this.edad = edad;
 	}
 	public void setIdioma(String idioma) {
-		//code here
+		/*
+		 * Entre estos valores (string):  inglés, español, alemán o francés.
+		 * No puede estar vacío.
+		 */
+		this.idioma = idioma;
 	}
 	
-
 	public String getIdioma() {
 		return idioma;
 	}
-	
 	public String getNombreJugador() {
 		return nombre;
 	}
 	public int getEdad() {
 		return edad;
 	}
+	
+	
 }
