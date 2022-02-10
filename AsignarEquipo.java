98f007c84b393e5e0ce74f7ffa680d28b8e41602
Juanjo2641 <juanjose.s264134@cesurformacion.com>
2022-02-10 10:04:34 +0100

    Añadidas funcionalidades a AsignarEquipo

--- AsignarEquipo.java
+++ AsignarEquipo.java
@@ -1,24 +1,43 @@
-
 public class AsignarEquipo {
-      private Equipo equipo;
-      private Jugador jugador;
-      
-  
-      
-      public void setEquipo(Equipo equipo) {
-  		//code here
-      }
-      
-      public void setJugador(Jugador jugador) {
-  		//code here
-      }
-      
-      public Equipo getEquipo(){
-          return equipo;
-      }
-      
-      public Jugador getJugador(){
-         return jugador;
-      }
-      
+	private Equipo equipo;
+	private Jugador jugador;
+	
+	
+	public AsignarEquipo() {
+
+	}
+
+	public void setEquipo(Equipo equipo) {
+		
+		this.equipo = null;
+		
+		if (
+		equipo.getNombreEquipo() != null 
+		&& equipo.getRanking() != -1) {
+			
+			this.equipo=equipo;
+		}
+	}
+
+	public void setJugador(Jugador jugador) {
+
+		this.jugador = null;
+		
+		if(
+		jugador.getNombreJugador() != null
+		&& jugador.getIdioma() != null
+		&& jugador.getEdad() != 0) {
+			
+			this.jugador=jugador;
+		}
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
 }
